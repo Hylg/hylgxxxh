ebac17cedf5a14f3c64136428b56948797428be0
rubmiss <269165678@qq.com>
2016-09-29 12:18:21 +0800

    init

---
+++ src/main/java/com/hylg/xxxh/utils/ResultCoreUtil.java
@@ -0,0 +1,18 @@
+package com.hylg.xxxh.utils;
+
+/**
+ * 返回值标识标准
+ * 
+ * @author Ruby
+ * @datatime 2016年9月29日上午9:42:48
+ */
+public class ResultCoreUtil {
+
+	public static final int SUCCESS = 1; // 成功
+
+	public static final int ERROR = 0; // 失败
+
+	public static final int NOT_LOGIN = 404; // 未登录
+	
+	public static final int EXCEPTION = 500; // 异常
+}
